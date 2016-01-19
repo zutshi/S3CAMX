974be1d09d7ee2183c3b641cafb76324bd3ce6c8
zutshi <zutshi@users.noreply.github.com>
2016-01-19 11:32:47 -0700

    Update set_path.sh

--- set_path.sh
+++ set_path.sh
@@ -1,6 +1,5 @@
-
-Z3_PATH=/home/zutshi/work/RA/cpsVerification/HyCU/z3_github/z3_installation/lib/python2.7/dist-packages
-PY2Z3_PATH=/home/zutshi/work/RA/cpsVerification/HyCU/symbSplicing/
+Z3_PATH=/home/re/RE_secam/z3_libs/
+PY2Z3_PATH=/home/re/RE_secam/
 CURR_PATH=$PWD
 
 # bleeding edge github checkout
