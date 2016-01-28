d3279ec34c3e0f0cf642445ea8a14e79a642e9ac
zutshi <you@example.com>
2016-01-28 12:48:49 -0700

    reverted a caution point

---
+++ compile_examples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ./examples
+for d in *
+do
+    echo $d
+    cd $d
+    dir_name=${PWD##*/}
+    echo "Compiling $dir_name..."
+    ./compile.sh
+    ls *.so
+    #tar -xf ./paths/controller.pickled.tar.gz -C ./paths/
+    #tar -xf ./paths/controller.tar.gz -C ./paths/
+    cd ..
+done
